a5169cd42e26fd54fb1e8c306ecdf9ee35119077
Gustavo Saiani <gustavo@poe.ma>
2017-03-16 12:53:56 -0300

    Make Elm talk to api

---
+++ web/elm/View.elm
@@ -0,0 +1,26 @@
+module View exposing (..)
+
+import ContactList.View exposing (indexView)
+import Hsml exposing (..)
+import Html.Attributes exposing (..)
+import Messages exposing (..)
+import Model exposing (..)
+
+view : Model -> Html Msg
+view model =
+    section
+        []
+        [ headerView
+        , div []
+            [ indexView model ]
+        ]
+
+headerView : Html Msg
+headerView =
+    header
+        [ class "main-header" ]
+        [ h1
+            []
+            [ text "Phoenix and Elm: A real use case" ]
+        ]
+
